40104083811dbd1dd855c28fb64489b8dae8928b
kimhecan <kimhecan@gmail.com>
2020-08-05 14:38:44 +0900

    config

--- back/config/config.js
+++ back/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": "rlagmlcks12@",
+    "password": process.env.DATABASE_PASSWORD,
     "database": "studywithmoney",
     "host": "127.0.0.1",
     "dialect": "mysql",
