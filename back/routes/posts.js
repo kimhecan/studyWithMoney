306760a70b8bbd1b99738db5864d9052aeddde58
kimhecan <kimhecan@gmail.com>
2020-08-12 17:35:02 +0900

    posts router update add function

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -5,41 +5,71 @@ const { Post, Image, User, Comment, ReComment } = require('../models');
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => { // loadPosts
-  try {
-    const where = { category: req.query.category };
-    if (parseInt(req.query.lastId, 10)) {
-      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }
-    }
-    const posts = await Post.findAll({
-      where,
-      limit: 10,
+const findPostByWhere = async where => {
+  const posts = await Post.findAll({
+    where,
+    limit: 7,
+    include: [{
+      model: User,
+      attributes: ['id', 'nickname', 'profileImg'],
+    }, {
+      model: Image,
+    }, {
+      model: Comment,
       include: [{
-        model: User, // 게시글 작성자
+        model: User,
         attributes: ['id', 'nickname', 'profileImg'],
       }, {
-        model: Image, // 게시글의 이미지
-      }, {
-        model: Comment, // 게시글의 댓글
+        model: ReComment,
         include: [{
-          model: User, //댓글을 쓴 사람
+          model: User,
           attributes: ['id', 'nickname', 'profileImg'],
-        }, {
-          model: ReComment,
-          include: [{
-            model: User, //대댓글을 쓴 사람
-            attributes: ['id', 'nickname', 'profileImg'],
-          }]
-        }],
-      }, {
-        model: User, // 좋아요 누른 사람
-        as: 'Likers',
-        attributes: ['id'],
+        }]
       }],
-      order: [
-        ['createdAt', 'DESC'],
-        [Comment, 'createdAt', 'DESC']
-      ],
+    }, {
+      model: User,
+      as: 'Likers',
+      attributes: ['id'],
+    }],
+    order: [
+      ['createdAt', 'DESC'],
+      [Comment, 'createdAt', 'DESC']
+    ],
+  });
+  return posts;
+}
+
+router.get('/', async (req, res, next) => { // loadPosts
+  try {
+    let posts = [];
+    if (req.query.category === 'all') {
+      console.log('al받음');
+      posts = posts.concat(await findPostByWhere({ category: 'freeBoard' }));
+      console.log(posts.length);
+      posts = posts.concat(await findPostByWhere({ category: 'infoBoard' }));
+      console.log(posts.length);
+      posts = posts.concat(await findPostByWhere({ category: 'clubBoard' }));
+      console.log(posts.length);
+    } else {
+      let where = { category: req.query.category };
+      if (parseInt(req.query.lastId, 10)) {
+        where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }
+      }
+      posts = posts.concat(await findPostByWhere(where));
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/main', async (req, res, next) => { // loadPosts
+  try {
+    const posts = await Post.findAll({
+      limit: 5,
+      order: ['createdAt', 'DESC'],
     });
 
     res.status(200).json(posts);
